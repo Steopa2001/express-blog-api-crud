38388036a4d3569b50be0375eb0f1ac57680d95a
Steopa2001 <moraru495@gmail.com>
2025-08-04 17:34:34 +0200

    feat: add getPostById function to fetch a post by ID with 404 error handling

--- controllers/postsControler.js
+++ controllers/postsControler.js
@@ -5,4 +5,16 @@ const getAllPosts = (req, res) => {
     res.json(posts);
 };
 
-//
+// Prendo un post per ID
+const getPostById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+
+    //Se non trovo il post, da errore 404
+    if(!post){
+        return res.status(404).json({ message: 'Post non trovato' });
+    }
+
+    //Ritorno il post trovato
+    res.json(post);
+}
